8cf775675a5519f537e16244aed59ae154d39960
Karen Meyer <kmeyer001@dundee.ac.uk>
2021-06-08 10:53:40 +0100

    Modified fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,12 @@
-def test_initial():
-    from fibonacci import fibonacci
+from functools import cache
 
-    assert fibonacci(0) == 0
-    assert fibonacci(1) == 1
+@cache
+def fibonacci(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+if __name__ == "__main__":
+    print(fibonacci(35))
